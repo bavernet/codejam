626e385483d3fc12c4791b994527ae5c17006dcb
Sungho Bae <bsh0236+contrib@gmail.com>
2015-09-21 21:08:31 +0900

    solve Project Euler #12 Highly divisible triangular number

--- ProjectEuler/012_HighlyDivisibleTriangularNumber/HighlyDivisibleTriangularNumber.cpp
+++ ProjectEuler/012_HighlyDivisibleTriangularNumber/HighlyDivisibleTriangularNumber.cpp
@@ -1,28 +1,38 @@
+// problem: https://projecteuler.net/problem=12
+// hint: math, implementation
+// level: easy
 #include <iostream>
-#include <limits>
+#include <vector>
 #include <cmath>
 
 #define MAX_T   (10)
 #define MAX_N   (1000)
+#define MAX_TRI (42000)
 
 using namespace std;
 
-int countDivisors(int64_t trinum) {
+inline int countDivisors(int trinum) {
 	int cnt = 0;
 	for(int i = 1, e = sqrt(trinum) + 1; i < e; ++i) {
 		if(trinum % i == 0) {
-			cnt += 1 + (trinum != (int64_t)i * i);
+			cnt += 1 + (trinum != i * i);
 		}
 	}
 	return cnt;
 }
 
-int64_t solve(int n) {
-	int64_t sum = 0;
-	for(int i = 1; i < numeric_limits<int>::max(); ++i) {
-		sum += i;
-		if(countDivisors(sum) > n)
-			return sum;
+int solve(int n) {
+	static int cache[MAX_TRI] = {0, };
+	static int last = 0;
+	static int tri = 0;
+	for(int i = last + 1; cache[i-1] <= n; ++i, ++last) {
+		tri += i;
+		cache[i] = countDivisors(tri);
+	}
+	for(int i = 1; i < MAX_TRI; ++i) {
+		if(cache[i] > n) {
+			return i * (i + 1) / 2;
+		}
 	}
 	return -1;
 }
