b93a08dc401358808492cb84a2d8f0f707f5ac78
Sungho Bae <bsh0236+contrib@gmail.com>
2015-09-24 02:07:40 +0900

    resolve Project Euler #15 Lattice paths using bottom-up method of dynamic programming

--- ProjectEuler/015_LatticePaths/LatticePathsWithMod.cpp
+++ ProjectEuler/015_LatticePaths/LatticePathsWithMod.cpp
@@ -1,3 +1,6 @@
+// problem: https://www.hackerrank.com/contests/projecteuler/challenges/euler015
+// hint: dp
+// level: easy
 #include <iostream>
 
 #define MAX_T   (1000)
@@ -7,25 +10,26 @@
 
 using namespace std;
 
-int cache[MAX_N+1][MAX_M+1] = { 0, };
+int cache[MAX_N+2][MAX_M+2] = { 0, };
+
+void findAllPaths(int n, int m) {
+	for(int i = 1; i < n + 2; ++i)
+		cache[i][1] = 1;
+	for(int j = 1; j < m + 2; ++j)
+		cache[1][j] = 1;
+	for(int i = 2; i < n + 2; ++i)
+		for(int j = 2; j < m + 2; ++j)
+			cache[i][j] = (cache[i-1][j] + cache[i][j-1]) % MOD;
+}
 
 int solve(int n, int m) {
-	if(n < 0 || m < 0)
-		return 0;
-
-	if(n == 0 || m == 0)
-		return 1;
-
-	int &nPaths = cache[n+1][m+1];
-	if(nPaths > 0)
-		return nPaths;
-
-	return (solve(n - 1, m) + solve(n, m - 1)) % MOD;
+	return cache[n+1][m+1];
 }
 
 int main(void) {
 	int nTests;
 	cin >> nTests;
+	findAllPaths(MAX_N, MAX_M);
 	while(nTests--) {
 		int n, m;
 		cin >> n >> m;
