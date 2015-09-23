e998bd3632a64539108a879cbb4a9f1191d91d62
Sungho Bae <bsh0236+contrib@gmail.com>
2015-09-24 02:09:30 +0900

    solve Project Euler #15 Lattice paths in projecteuler.net

---
+++ ProjectEuler/015_LatticePaths/LatticePaths.cpp
@@ -0,0 +1,38 @@
+// problem: https://projecteuler.net/problem=15
+// hint: dp
+// level: easy
+#include <iostream>
+
+#define MAX_T   (1000)
+#define MAX_N   (500)
+#define MAX_M   (500)
+
+using namespace std;
+
+int64_t cache[MAX_N+2][MAX_M+2] = { 0, };
+
+void findAllPaths(int n, int m) {
+	for(int i = 1; i < n + 2; ++i)
+		cache[i][1] = 1;
+	for(int j = 1; j < m + 2; ++j)
+		cache[1][j] = 1;
+	for(int i = 2; i < n + 2; ++i)
+		for(int j = 2; j < m + 2; ++j)
+			cache[i][j] = cache[i-1][j] + cache[i][j-1];
+}
+
+int64_t solve(int n, int m) {
+	return cache[n+1][m+1];
+}
+
+int main(void) {
+	int nTests;
+	cin >> nTests;
+	findAllPaths(MAX_N, MAX_M);
+	while(nTests--) {
+		int n, m;
+		cin >> n >> m;
+		cout << solve(n, m) << endl;
+	}
+	return 0;
+}
