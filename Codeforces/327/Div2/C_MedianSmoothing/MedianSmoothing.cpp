006395177a610d433495c243c2b5496ca98d323a
Sungho Bae <bsh0236@gmail.com>
2015-10-28 12:28:07 +0900

    solve Codeforces Round #327 Div2 C. Median Smoothing

---
+++ Codeforces/327/Div2/C_MedianSmoothing/MedianSmoothing.cpp
@@ -0,0 +1,61 @@
+// problem: http://codeforces.com/contest/586/problem/C
+// hint: greedy, implementation
+// level: easy
+#include <iostream>
+#include <vector>
+
+#define MAX_N   (500000)
+
+using namespace std;
+
+inline void fill(vector<bool> &a, int si, int ei) {
+	for( ; si <= ei; ++si, --ei) {
+		a[si] = a[si-1];
+		a[ei] = a[ei+1];
+	}
+}
+
+vector<bool> solve(vector<bool> a, int &maxDepth) {
+	vector<bool> diff(a.size()), result(a.begin(), a.end());
+	for(int i = 0; i + 1 < a.size(); ++i) {
+		diff[i] = (a[i] != a[i+1]);
+	}
+
+	int cnt = 0, startIdx = 0;
+	maxDepth = 0;
+	for(int i = 0; i < diff.size(); ++i) {
+		if(diff[i]) {
+			if(cnt == 0)
+				startIdx = i;
+			++cnt;
+		} else {
+			maxDepth = max(maxDepth, cnt / 2);
+			fill(result, startIdx + 1, startIdx + cnt - 1);
+			cnt = 0;
+		}
+	}
+
+	return result;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+
+	vector<bool> a;
+	for(int i = 0; i < n; ++i) {
+		bool b;
+		cin >> b;
+		a.push_back(b);
+	}
+
+	int depth;
+	vector<bool> result = solve(a, depth);
+	cout << depth << endl;
+	for(bool item : result) {
+		cout << item << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
