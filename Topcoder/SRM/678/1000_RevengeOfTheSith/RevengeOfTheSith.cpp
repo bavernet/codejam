5436b204f6bf3c2519cf0d63d77b86563220324f
Sungho Bae <bsh0236@gmail.com>
2016-02-16 19:25:07 +0900

    modify Topcoder SRM #678 Div2 RevengeOfTheSith(1000P) problem by dp

--- Topcoder/SRM/678/1000_RevengeOfTheSith/RevengeOfTheSith.cpp
+++ Topcoder/SRM/678/1000_RevengeOfTheSith/RevengeOfTheSith.cpp
@@ -1,5 +1,5 @@
 // problem: https://community.topcoder.com/stat?c=problem_statement&pm=13191&rd=16648
-// hint: brute-force
+// hint: 
 // level: moderate
 #include <iostream>
 #include <vector>
@@ -9,32 +9,47 @@ using namespace std;
 
 class RevengeOfTheSith {
 private:
-	int getHarmPoints(const vector<int> &steps, int D) {
-		int points = 0;
-		for(int height : steps) {
-			if(D < height) {
-				points += (height - D) * (height - D);
-			}
-		}
-		return points;
+	int cost(int height, int space, int D) {
+		int lh = max(0, height / space - D);
+		int hh = max(0, height / space + 1 - D);
+		int hc = height % space;
+		int lc = space - hc;
+		return lh * lh * lc + hh * hh * hc;
 	}
 public:
 	int move(vector<int> steps, int T, int D) {
-		if(T == 0)
-			return getHarmPoints(steps, D);
-
-		vector<int> s2(steps.begin(), steps.end());
-		int answer = INT_MAX;
-		for(int i = 0; i + 1 < s2.size(); ++i) {
-			int limit = steps[i] + steps[i+1];
-			for(int h = 1; h < limit; ++h) {
-				s2[i] = h;
-				s2[i+1] = steps[i] + steps[i+1] - h;
-				answer = min(answer, move(s2, T - 1, D));
-			}
-			s2[i] = steps[i];
+		int N = steps.size();
+		vector<int> height(N);
+		height[0] = steps[0];
+		for(int i = 1; i < height.size(); ++i) {
+			height[i] = height[i-1] + steps[i];
 		}
 
+		vector<vector<int> > dp(N + 1, vector<int>(N + 1));
+		for(int t = 0; t <= T; ++t) {
+			for(int r = 1; r <= N; ++r) {
+				for(int l = 0; l < r; ++l) {
+					:q
+				}
+			}
+		}
+		dp[0][T] = 0;
+		for(int r = 1; r <= N; ++r) {
+			for(int t = 0; t <= T; ++t) {
+				for(int l = 0; l < r; ++l) {
+					int mov = r - l - 1;
+					if(mov <= t) {
+						dp[r][t-mov] = min(dp[r][t-mov],
+							dp[l][t] + cost(height[r] - height[l], r - l, D));
+					}
+				}
+			}
+		}
+
+		int answer = INT_MAX;
+		for(int t = 0; t <= T; ++t) {
+			answer = min(answer, dp[N][t]);
+		}
 		return answer;
 	}
 };
