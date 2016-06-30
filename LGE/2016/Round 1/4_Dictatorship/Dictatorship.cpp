c23110adbc3a081489eaad5eadda08629092f912
Sungho Bae <bsh0236@gmail.com>
2016-07-01 02:30:03 +0900

    public codejam 2016 #4 Dictatorship using dijkstra for newly add edges

--- LGE/2016/Round 1/4_Dictatorship/Dictatorship.cpp
+++ LGE/2016/Round 1/4_Dictatorship/Dictatorship.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
+#include <queue>
+#include <functional>
+#include <utility>
 
 using namespace std;
 
@@ -9,7 +13,6 @@ using namespace std;
 int parent[MAX_N+1];
 int depth[MAX_N+1];
 int cost[MAX_N+1];
-int cache[MAX_N+1];
 
 inline int getCommonAncestor(int a, int b) {
 	while (depth[a] > depth[b]) a = parent[a];
@@ -21,20 +24,30 @@ inline int getCommonAncestor(int a, int b) {
 	return a;
 }
 
-int getCost(map<int,int> &leaf, int v) {
-	int &c = cache[v];
-	if (c > 0)
-		return c;
+void dijkstra(vector<map<int,int> > &g, vector<int> &d, int s) {
+	priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > > pq;
+	vector<bool> visited(d.size());
 
-	c = cost[v];
-	for (const auto &p : leaf) {
-		if (c <= p.second)
+	d[s] = 0;
+	pq.push(make_pair(0, s));
+	while (!pq.empty()) {
+		pair<int,int> p = pq.top();
+		pq.pop();
+
+		int u = p.second;
+		if (visited[u])
 			continue;
-		int ancestor = getCommonAncestor(v, p.first);
-		int dist = cost[v] + cost[p.first] - cost[ancestor] * 2;
-		c = min(c, dist + p.second);
+
+		visited[u] = true;
+		for (const auto &adj : g[u]) {
+			int v = adj.first;
+			int c = adj.second;
+			if (d[v] > d[u] + c) {
+				d[v] = d[u] + c;
+				pq.push(make_pair(d[v], v));
+			}
+		}
 	}
-	return c;
 }
 
 int main(void) {
@@ -46,24 +59,32 @@ int main(void) {
 		int N, Q;
 		cin >> N >> Q;
 
-		map<int,int> leaf;
+		vector<map<int,int> > g(N + 1);
+		set<int> leaf;
 		cost[1] = 0;
 		depth[1] = 1;
-		for (int i = 1; i < N; ++i) {
+		for (int i = 2; i <= N; ++i) {
 			int P, C;
 			cin >> P >> C;
-			parent[i+1] = P;
-			cost[i+1] = cost[P] + C;
-			depth[i+1] = depth[P] + 1;
+			g[P][i] = g[i][P] = C;
+			parent[i] = P;
+			cost[i] = cost[P] + C;
+			depth[i] = depth[P] + 1;
 			leaf.erase(P);
-			leaf[i + 1] = 0;
+			leaf.insert(i);
 		}
 
-		for (auto &p : leaf) {
-			cin >> p.second;
+		for (const int &v : leaf) {
+			int nc;
+			cin >> nc;
+			if (g[v][1])
+				nc = min(g[v][1], nc);
+			g[1][v] = g[v][1] = nc;
 		}
 
-		memset(cache, 0, sizeof(int) * (N + 1));
+		vector<int> d(N + 1, INT_MAX);
+		dijkstra(g, d, 1);
+
 		long long before = 0, after = 0;
 		for (int i = 0; i < Q; ++i) {
 			int a, b;
@@ -72,10 +93,7 @@ int main(void) {
 			int ancestor = getCommonAncestor(a, b);
 			int dist = cost[a] + cost[b] - cost[ancestor] * 2;
 			before += dist;
-
-			int costA = getCost(leaf, a);
-			int costB = getCost(leaf, b);
-			after += min(dist, costA + costB);
+			after += min(dist, d[a] + d[b]);
 		}
 
 		cout << before << " " << after << endl;
