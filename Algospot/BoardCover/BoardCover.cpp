e857ba6119f32dba2510c9045509934308b287e2
Sungho Bae <bsh0236@gmail.com>
2016-10-07 14:15:54 +0900

    algospot: refact the solution of BoardCover problem

--- Algospot/BoardCover/BoardCover.cpp
+++ Algospot/BoardCover/BoardCover.cpp
@@ -15,51 +15,45 @@ int block[4][3][2] = {
 	{ { 0, 0 }, { 0, 1 }, { -1, 1 } },
 };
 
-int set(int r, int c, int cnt, int type, vector<string> &bd) {
-	int h = bd.size() - 1, w = bd[0].size() - 1;
-	int ans = 0;
+bool check(int r, int c, int type, vector<string> &bd) {
 	for (int i = 0; i < 3; ++i) {
 		int x = block[type][i][0];
 		int y = block[type][i][1];
-		if (x + c < 0) return 0;
-		if (bd[y+r][x+c] != '.') {
-			return 0;
-		}
+		if (x + c < 0 || bd[y+r][x+c] != '.')
+			return false;
 	}
+	return true;
+}
+
+void set(int r, int c, int type, char ch, vector<string> &bd) {
+	for (int i = 0; i < 3; ++i) {
+		int x = block[type][i][0];
+		int y = block[type][i][1];
+		bd[y+r][x+c] = ch;
+	}
+}
+
+int lay(int r, int c, int cnt, int type, vector<string> &bd) {
+	int h = bd.size() - 1, w = bd[0].size() - 1;
+	int ans = 0;
+	if (!check(r, c, type, bd))
+		return 0;
 
 	cnt -= 3;
 	if (!cnt)
 		return 1;
+	set(r, c, type, '*', bd);
 
-	for (int i = 0; i < 3; ++i) {
-		int x = block[type][i][0];
-		int y = block[type][i][1];
-		bd[y+r][x+c] = '*';
-	}
-
-	bool change = false;
 	for (int i = r; i < h; ++i) {
 		for (int j = ((i == r)? c: 0); j < w; ++j) {
 			if (bd[i][j] == '.') {
-				for (int t = 0; t < 4; ++t) {
-					ans += set(i, j, cnt, t, bd);
-				}
-				change = true;
-				break;
+				for (int t = 0; t < 4; ++t)
+					ans += lay(i, j, cnt, t, bd);
+				set(r, c, type, '.', bd);
+				return ans;
 			}
 		}
-		if (change) {
-			break;
-		}
 	}
-
-	for (int i = 0; i < 3; ++i) {
-		int x = block[type][i][0];
-		int y = block[type][i][1];
-		bd[y+r][x+c] = '.';
-	}
-
-	cnt += 3;
 	return ans;
 }
 
@@ -72,9 +66,8 @@ int cases(int cnt, vector<string> &bd) {
 		for (int j = 0; j < w; ++j) {
 			if (bd[i][j] == '.') {
 				int ans = 0;
-				for (int t = 0; t < 4; ++t) {
-					ans += set(i, j, cnt, t, bd);
-				}
+				for (int t = 0; t < 4; ++t)
+					ans += lay(i, j, cnt, t, bd);
 				return ans;
 			}
 		}
