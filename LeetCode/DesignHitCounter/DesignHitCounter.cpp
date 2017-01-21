19a2945892854451afc31f3756a5d3b2fe6ba878
Sungho Bae <bsh0236@gmail.com>
2017-01-21 23:00:26 +0900

    leetcode: redesign `Design Hit Counter` problem in terms of concurrency

--- LeetCode/DesignHitCounter/DesignHitCounter.cpp
+++ LeetCode/DesignHitCounter/DesignHitCounter.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
-#include <queue>
+#include <set>
 #include <atomic>
 #include <cstring>
-
-#define PERIOD_MAX  (5*60)
+#include <climits>
 
 using namespace std;
 
 class HitCounter {
 private:
-	int total;
+	static constexpr int PERIOD_MAX = 5*60;
+	int lastCount, lastTimestamp;
+	set<pair<int,int> > timeline;
 	atomic_flag lock;
-	queue<pair<int,int> > timeline; /* (timestamp, count) */
-
-	void update(int timestamp) {
-		int low = timestamp - PERIOD_MAX;
-		while (!timeline.empty() && timeline.front().first <= low) {
-			total -= timeline.front().second;
-			timeline.pop();
-		}
-	}
 
 public:
 	/** Initialize your data structure here. */
-	HitCounter(): total(0), lock(ATOMIC_FLAG_INIT) {
+	HitCounter(): lastCount(0), lastTimestamp(INT_MIN), lock(ATOMIC_FLAG_INIT) {
 	}
 
 	/** Record a hit.
 	 * @param timestamp - The current timestamp (in seconds granularity). */
 	void hit(int timestamp) {
+		int prevCount, prevTimestamp;
+		bool turned = false;
+
 		while (lock.test_and_set(memory_order_acquire))
 			;
 
-		update(timestamp);
-
-		if (!timeline.empty() && timeline.back().first == timestamp)
-			++timeline.back().second;
-		else
-			timeline.push(make_pair(timestamp, 1));
-
-		++total;
+		if (lastTimestamp < timestamp) {
+			prevCount = lastCount;
+			prevTimestamp = lastTimestamp;
+			lastTimestamp = timestamp;
+			turned = true;
+		}
+		++lastCount;
 
 		lock.clear(memory_order_release);
+
+		if (turned) {
+			timeline.insert(timeline.rbegin().base(), make_pair(prevTimestamp, prevCount));
+			if (timeline.size() > PERIOD_MAX + 1)
+				timeline.erase(timeline.begin());
+		}
 	}
 
 	/** Return the number of hits in the past 5 minutes.
@@ -51,8 +51,20 @@ public:
 		while (lock.test_and_set(memory_order_acquire))
 			;
 
-		update(timestamp);
-		ans = total;
+		if (timestamp - PERIOD_MAX < lastTimestamp) {
+			auto it = timeline.lower_bound(make_pair(timestamp - PERIOD_MAX + 1, INT_MIN));
+			if (it == timeline.begin()) {
+				ans = lastCount;
+			} else {
+				--it;
+				ans = lastCount - it->second;
+				timeline.erase(timeline.begin(), it);
+			}
+		} else {
+			timeline.clear();
+			ans = lastCount = 0;
+			lastTimestamp = timestamp;
+		}
 
 		lock.clear(memory_order_release);
 		return ans;
