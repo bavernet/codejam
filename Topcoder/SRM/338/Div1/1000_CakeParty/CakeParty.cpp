924a55393b6ff8dc2342bab1026e5e941d8d9821
Sungho Bae <bsh0236@gmail.com>
2018-03-13 00:11:47 +0900

    topcoder: modify solution for Div1 `Cake Party` problem(1000p) in Round #338

--- Topcoder/SRM/338/Div1/1000_CakeParty/CakeParty.cpp
+++ Topcoder/SRM/338/Div1/1000_CakeParty/CakeParty.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
+struct strcomp {
+	bool operator ()(const int &a, const int &b) {
+		return to_string(a) < to_string(b);
+	}
+};
+
 class CakeParty {
 public:
-	string makeMove(vector <int> pieces) {
-		int c = 0, max1 = pieces[0], max2 = 0;
-		for (int i = 1; i < pieces.size(); ++i) {
-			if (max1 == pieces[i]) {
-				if (to_string(i) < to_string(c))
-					c = i;
-				max2 = max1;
-			} else if (max1 < pieces[i]) {
-				c = i;
-				max2 = max1;
-				max1 = pieces[i];
-			} else if (max2 < pieces[i]) {
-				max2 = pieces[i];
+	string makeMove(vector<int> pieces) {
+		int n = pieces.size();
+		map<int,set<int,strcomp> > m;
+		for (int i = 0; i < n; ++i)
+			m[pieces[i]].insert(i);
+
+		int x = 0;
+		for (auto sz : pieces)
+			x ^= sz;
+
+		auto it = m.rbegin();
+		int cake = *it->second.begin();
+		int max1 = it->first;
+		int target = x ^ max1;
+		if (!x || max1 < target)
+			return "CAKE " + to_string(cake) + " PIECES 1";
+
+		set<int,strcomp> s;
+		s.insert(max1 - target);
+		cout << "ans: " << (max1 - target) << endl;
+		int max2 = (it->second.size() > 1)? max1: (++it)->first;
+		for (int i = 1, e = target; i <= e; ++i) {
+			int nx = target ^ i;
+			int nt = nx ^ max2;
+			if (max2 <= nt) {
+				s.insert(max1 - i);
+				cout << "i: " << i << endl;
 			}
 		}
+		return "CAKE " + to_string(cake) + " PIECES " + to_string(*s.begin());
 
+#if 0
 		int x = 0;
 		for (auto sz : pieces)
 			x ^= sz;
@@ -36,6 +61,7 @@ public:
 			if (to_string(i) < to_string(p))
 				p = i;
 		return "CAKE " + to_string(c) + " PIECES " + to_string(p);
+#endif
 	}
 };
 
