0153a9e1d049a73c0cbc2c94ac803691dd4c0423
Sungho Bae <bsh0236@gmail.com>
2019-06-09 17:20:20 +0900

    Geeks for Geeks: update the algorithm of sieve of eratosthenes
    The complexity of simple sieve of eratosthenes is O(N*lgN).
    This is a simple way to improve the algorithm in terms of the complexity
    from O(N*lgN) to O(N*lg(lgN)).

--- GeeksForGeeks/SieveOfEratosthenes/SieveOfEratosthenes.cpp
+++ GeeksForGeeks/SieveOfEratosthenes/SieveOfEratosthenes.cpp
@@ -15,7 +15,7 @@ int main(void) {
 	for (int i = 2; i < MAX_N; ++i) {
 		if (!prime[i])
 			continue;
-		for (int j = i + i; j < MAX_N; j += i)
+		for (int j = i * i; j < MAX_N; j += i)
 			prime[j] = false;
 	}
 
